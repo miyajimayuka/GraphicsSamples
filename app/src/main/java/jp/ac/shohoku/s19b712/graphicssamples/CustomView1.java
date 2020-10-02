b441f203f21fa82f92ea0a5860accaa602b82069
s19B712 <s19b712@shohokuac.jp>
2020-10-02 12:19:54 +0900

    テスト

--- app/src/main/java/jp/ac/shohoku/s19b712/graphicssamples/CustomView1.java
+++ app/src/main/java/jp/ac/shohoku/s19b712/graphicssamples/CustomView1.java
@@ -1,21 +1,73 @@
 package jp.ac.shohoku.s19b712.graphicssamples;
 
+//import android.content.Context;
+//import android.util.AttributeSet;
+//import android.view.View;
+
+//import androidx.annotation.Nullable;
+
+//public class CustomView1 extends View {
+    //public CustomView1(Context context) {
+        //super(context);
+    //}
+
+    //public CustomView1(Context context, @Nullable AttributeSet attrs) {
+        //super(context, attrs);
+    //}
+
+    //public CustomView1(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        //super(context, attrs, defStyleAttr);
+    //}
+
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
-import androidx.annotation.Nullable;
-
+/**
+ * Created by  on 2015/10/05.
+ * Android 端末でグラフィックス表示をするためのサンプルです．
+ */
 public class CustomView1 extends View {
+    //コンストラクタ 1
     public CustomView1(Context context) {
         super(context);
     }
-
-    public CustomView1(Context context, @Nullable AttributeSet attrs) {
+    //コンストラクタ 2
+    public CustomView1(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
-
-    public CustomView1(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+    //コンストラクタ 3
+    public CustomView1(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
     }
+
+    /**
+     * このメソッドで描画をします．
+     * @param canvas 画像表示用のキャンバス
+     */
+    protected void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+        canvas.drawColor(Color.WHITE);
+
+        int w = this.getWidth();
+        int h = this.getHeight();
+        Paint p = new Paint();
+        p.setStyle(Style.STROKE); //Style を設定: 線で描く
+        p.setColor(Color.DKGRAY); //色を設定
+        canvas.drawRect(new Rect(10, 10, w - 50, h - 50), p); //長方形を描画
+        for(int i = 0; i<10; i++){
+            Paint p2 = new Paint();
+            p2.setStyle(Style.FILL); //Style を設定： 塗りつぶす
+            p2.setColor(Color.rgb(25*i, 0, 0)); //色を設定
+            canvas.drawCircle(25*i+125, 25*i+125, 100,p2); //円を描く
+        }
+    }
+
+}
+
 }
--- app/src/main/java/jp/ac/shohoku/s19b712/graphicssamples/CustomView2.java
+++ app/src/main/java/jp/ac/shohoku/s19b712/graphicssamples/CustomView2.java
@@ -1,21 +1,52 @@
 package jp.ac.shohoku.s19b712.graphicssamples;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
-import androidx.annotation.Nullable;
-
-public class CustomView2 extends View {
-    public CustomView2(Context context) {
+ /**
+ * Created by  on 2015/10/05.
+ * Android 端末でグラフィックス表示をするためのサンプルです．
+ */
+        public class CustomView2 extends View {
+ //コンストラクタ 1
+        public CustomView2(Context context) {
         super(context);
-    }
-
-    public CustomView2(Context context, @Nullable AttributeSet attrs) {
+        }
+//コンストラクタ 2
+        public CustomView2(Context context, AttributeSet attrs) {
         super(context, attrs);
-    }
+        }
+//コンストラクタ 3
+        public CustomView2(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        }
 
-    public CustomView2(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
-}
+ /**
+ * このメソッドで描画をします．
+ * @param canvas 画像表示用のキャンバス
+ */
+        protected void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+        canvas.drawColor(Color.WHITE);
+
+        int w = this.getWidth();
+        int h = this.getHeight();
+                Paint p = new Paint();
+        p.setStyle(Style.STROKE); //Style を設定: 線で描く
+        p.setColor(Color.DKGRAY); //色を設定
+        canvas.drawRect(new Rect(5, 5, w - 10, h - 10), p); //長方形を描画
+                for(int i = 0; i<10; i++){
+            Paint p2 = new Paint();
+            p2.setStyle(Style.FILL); //Style を設定： 塗りつぶす
+            p2.setColor(Color.rgb(25*i, 0, 0)); //色を設定
+            canvas.drawCircle(25*i+125, 25*i+125, 100,p2); //円を描く
+            }
+         }
+
+         }
